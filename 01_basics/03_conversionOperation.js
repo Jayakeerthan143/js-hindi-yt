341fbd2ca6879c46c8da8c8e9fdd03e9a7c874a0
Jayakeerthan143 <jayakeerthan143@gmail.com>
2025-08-06 15:37:02 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33jkm"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// NOTES
+// "33" => 33
+// "33abc" or "jay" => NaN
+// true => 1 ; false => 0;
+// null =>0 ; undefined => NaN
+
+let isLoggedIn = 123;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; "jay" => true
+// 0 => false ; "" => false
+
+let someNumber = 48
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
