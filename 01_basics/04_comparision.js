7a39babb90c791dcdea02e97803270b6cdb6c8a4
Jayakeerthan143 <jayakeerthan143@gmail.com>
2025-08-06 16:18:55 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log("2" > 1); // output => true
+// console.log("02" > 1); // output => true 
+
+// console.log( null > 0); // output => false
+// console.log(null == 0); // output => false
+// console.log(null >= 0); // output => true
+// beacuse comparisions convert null to a number treating it as 0 , but equality check dont == 
+
+
+// console.log(undefined == 0); // output => false
+// console.log(undefined > 0);  // output => false
+// console.log(undefined < 0); // output => false
+
+// === ; strict check 
+
+// here it should match the data type also 
+
+console.log("2" === 2); // output => false
+
+
+
+
+
